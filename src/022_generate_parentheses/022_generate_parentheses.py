121d73429f62d3566b875946337eab1b0ab6ffaf
ypliu <liuyanpo@yeah.net>
2018-09-04 22:47:38 +0800

    Create 022_generate_parentheses.py

---
+++ src/022_generate_parentheses/022_generate_parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        self.generate_dfs(n, n, "", res)
+        return res
+
+    def generate_dfs(self, num_left, num_right, cur_comb, res):
+        if num_left == 0:
+            cur_comb += ")" * num_right
+            res.append(cur_comb)
+            return
+        self.generate_dfs(num_left-1, num_right, cur_comb+"(", res)
+        if num_left < num_right:
+            self.generate_dfs(num_left, num_right-1, cur_comb+")", res)
+
+#
+s = Solution()
+print s.generateParenthesis(3)
