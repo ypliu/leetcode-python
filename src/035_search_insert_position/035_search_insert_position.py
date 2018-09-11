41465670643d4450df8b251a489b5ae564e62231
ypliu <liuyanpo@yeah.net>
2018-09-11 22:39:22 +0800

    Create 035_search_insert_position.py

---
+++ src/035_search_insert_position/035_search_insert_position.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        left, right = 0, len(nums)-1
+        if target <= nums[left]:
+            return left
+        elif target > nums[right]:
+            return (right + 1)
+
+        while left < right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return right
+
+# debug
+s = Solution()
+print s.searchInsert([1,3,5,6], 0)
+print s.searchInsert([1,3,5,6], 2)
+print s.searchInsert([1,3,5,6], 5)
+print s.searchInsert([1,3,5,6], 7)
