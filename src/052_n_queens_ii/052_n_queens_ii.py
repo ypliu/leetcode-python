7bde5d6b7bd13360fe03273098aa409fa18f6464
ypliu <liuyanpo@yeah.net>
2018-09-20 00:51:27 +0800

    Create 052_n_queens_ii.py

---
+++ src/052_n_queens_ii/052_n_queens_ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+
+        if 0 == n:
+            return 0
+        return self.dfs(n, [], [], [])
+
+    def dfs(self, num_queens, col_occupied, diag_sub_occupied, diag_add_occupied):
+        res, row_th = 0, len(col_occupied)
+        if num_queens == row_th:
+            return 1
+
+        for col_th in range(num_queens):
+            rc_sub, rc_add = row_th - col_th, row_th + col_th
+            if (col_th in col_occupied) or (rc_sub in diag_sub_occupied) or (rc_add in diag_add_occupied):
+                continue
+            res += self.dfs(num_queens, col_occupied+[col_th], diag_sub_occupied+[rc_sub], diag_add_occupied+[rc_add])
+        return res
+
+# debug
+s = Solution()
+print s.solveNQueens(4)
