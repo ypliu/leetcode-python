16576b67d1452d08f417ef6d4270d016b12a31e2
ypliu <liuyanpo@yeah.net>
2018-11-09 11:40:32 +0800

    Create 137_single_number_ii.py

---
+++ src/137_single_number_ii/137_single_number_ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # return (sum(set(nums))*3 - sum(nums)) // 2
+        if (not nums) or (len(nums)%3 != 1):
+            return None
+        residue = 0; carry = 0
+        for n in nums:
+            residue = (residue ^ n) & (~carry)
+            carry = (carry ^ n) & (~residue)
+        return residue
+
+# debug
+s = Solution()
+print s.singleNumber([2,2,3,2])
+print s.singleNumber([0,1,0,1,0,1,99])
