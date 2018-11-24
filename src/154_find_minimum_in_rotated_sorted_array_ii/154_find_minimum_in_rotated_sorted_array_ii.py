2c9a3b29472cabc4dd21f1a7e2d5ec76d87a3620
ypliu <liuyanpo@yeah.net>
2018-11-24 15:10:34 +0800

    Create 154_find_minimum_in_rotated_sorted_array_ii.py

---
+++ src/154_find_minimum_in_rotated_sorted_array_ii/154_find_minimum_in_rotated_sorted_array_ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if (not nums) or (0 == len(nums)):
+            return None
+        low = 0; high = len(nums)-1
+        while low < high:
+            if nums[low] < nums[high]:
+                return nums[low]
+            mid = (high - low) // 2 + low
+            if nums[low] < nums[mid]:
+                low = mid + 1
+            elif nums[low] > nums[mid]:
+                low += 1
+                high = mid
+            else:
+                low += 1
+        return nums[low]
+
+# debug
+s = Solution()
+print s.findMin([3,4,5,1,2])
+print s.findMin([4,5,6,7,0,1,2])
+print s.findMin([1,3,5])
+print s.findMin([2,2,2,0,1])
