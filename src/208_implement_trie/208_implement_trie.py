5ec6de93d5c31ee35f98067d72e5df82fc452f1b
ypliu <liuyanpo@yeah.net>
2018-12-14 21:09:52 +0800

    Create 208_implement_trie.py

---
+++ src/208_implement_trie/208_implement_trie.py
@@ -0,0 +1,60 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.is_word
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
+
+# debug
+# Your Trie object will be instantiated and called as such:
+obj = Trie()
+obj.insert("apple")
+print obj.search("apple")
+print obj.search("app")
+print obj.startsWith("app")
+obj.insert("app")
+print obj.search("app")
