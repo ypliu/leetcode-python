218a980f04d1eaa749c9aead586f818a0ca82689
ypliu <liuyanpo@yeah.net>
2018-12-28 23:15:19 +0800

    Create 227_basic_calculator_ii.py

---
+++ src/227_basic_calculator_ii/227_basic_calculator_ii.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        def eval(n1, n2, op):
+            if ('+' == op):
+                return (n1 + n2)
+            if ('-' == op):
+                return (n1 - n2)
+            if ('*' == op):
+                return (n1 * n2)
+            if ('/' == op):
+                return (n1 / n2)
+
+        stack = []; n = 0
+        for ch in (s+'+'):
+            if (' ' == ch):
+                continue
+            elif ('0' <= ch <= '9'):
+                n = n * 10 + ord(ch) - ord('0')
+            elif ('*' == ch) or ('/' == ch):
+                if (len(stack) >= 2) and (stack[-1] in "*/"):
+                    op = stack.pop()
+                    n1 = stack.pop()
+                    n = eval(n1, n, op)
+                stack.append(n)
+                stack.append(ch)
+                n = 0
+            elif ('+' == ch) or ('-' == ch):
+                while (len(stack) >= 2):
+                    op = stack.pop()
+                    n1 = stack.pop()
+                    n = eval(n1, n, op)
+                stack.append(n)
+                stack.append(ch)
+                n = 0
+            else:
+                print "Error! Fake char:", ch
+        return stack[0]
+
+# debug
+s = Solution()
+print s.calculate("3+2*2")
+print s.calculate("3/2")
+print s.calculate(" 3+5 / 2 ")
